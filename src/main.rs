b8d5cead7957f8f5b5d0d2a76c6b47c474a56c8b
yydcnjjw <yydcnjjw@gmail.com>
2020-11-19 17:31:37 +0800

    qt gui support

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 mod command;
 mod util;
 
-use SubCommand::Dict;
-use SubCommand::Translate;
 use clap::Clap;
 use command::{Opts, SubCommand};
-use gio::prelude::*;
-use gtk::prelude::*;
+use SubCommand::Dict;
+use SubCommand::Translate;
+
+use qt_core::{q_init_resource, qs};
+use qt_widgets::QApplication;
+use qt_qml::QQmlApplicationEngine;
 
 #[tokio::main]
 async fn main() {
-    match Opts::parse().subcmd {
-        Dict(dict) => dict.do_query().await,
-        Translate(translate) => translate.do_query().await,
-    }
+    QApplication::init(|_| unsafe {
+        q_init_resource!("resources");
+        let _engine = QQmlApplicationEngine::from_q_string(&qs("qrc:/main.qml"));
+        QApplication::exec()
+    })
+
+    // match Opts::parse().subcmd {
+    //     Dict(dict) => dict.do_query().await,
+    //     Translate(translate) => translate.do_query().await,
+    // }
 }
