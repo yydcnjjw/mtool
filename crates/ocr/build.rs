707654dec23de595cde70b97ac826b15da8f020a
yydcnjjw <yydcnjjw@gmail.com>
2021-07-29 08:34:21 +0800

    update

--- crates/ocr/build.rs
+++ crates/ocr/build.rs
@@ -1,9 +1,9 @@
 fn main() {
+    cxx_build::bridge("src/lib.rs")
+        .compile("screen");
+
     let dst = cmake::build("screenshot");
 
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=dylib=screenshot");
-
-    cxx_build::bridge("src/lib.rs")
-        .compile("screen")
 }
