262194ba946960c859620730a208fc525c31c0da
yydcnjjw <yydcnjjw@gmail.com>
2021-11-29 08:51:15 +0800

    update

--- crates/sysev/src/windows/mod.rs
+++ crates/sysev/src/windows/mod.rs
@@ -1,11 +1,10 @@
-use std::{thread, time::Duration};
+use std::ptr::null_mut;
 
 use thiserror::Error;
 use windows::Win32::{
     Foundation::{HINSTANCE, HWND, LPARAM, LRESULT, WPARAM},
     UI::WindowsAndMessaging::{
-        CallNextHookEx, DispatchMessageW, PeekMessageW, SetWindowsHookExW, TranslateMessage, HHOOK,
-        MSG, PM_REMOVE, WH_KEYBOARD, WH_KEYBOARD_LL,
+        CallNextHookEx, GetMessageW, SetWindowsHookExW, HHOOK, KBDLLHOOKSTRUCT, WH_KEYBOARD_LL,
     },
 };
 
@@ -19,13 +18,19 @@ type Result<T> = std::result::Result<T, Error>;
 
 static mut HHK: HHOOK = HHOOK(0);
 
-unsafe extern "system" fn keyboard_hook(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
-    CallNextHookEx(HHK, code, wparam, lparam)
+extern "system" fn keyboard_hook(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    let ev = unsafe { *(lparam.0 as *const KBDLLHOOKSTRUCT) };
+    let action = wparam.0;
+
+    println!("{:?}, {:?}", action, ev);
+
+    unsafe { CallNextHookEx(HHK, code, wparam, lparam) }
 }
 
 fn run() -> Result<()> {
     unsafe {
         HHK = SetWindowsHookExW(WH_KEYBOARD_LL, Some(keyboard_hook), HINSTANCE::default(), 0);
+        GetMessageW(null_mut(), HWND::default(), 0, 0);
     }
 
     Ok(())
