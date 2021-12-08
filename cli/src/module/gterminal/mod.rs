7e0bf416aa649056372ff36df68c167e44070b0d
yydcnjjw <yydcnjjw@gmail.com>
2021-12-08 20:29:26 +0800

    feat(gterminal): iced wgpu integration

--- cli/src/module/gterminal/mod.rs
+++ cli/src/module/gterminal/mod.rs
@@ -6,82 +6,105 @@ use crate::app::App;
 use controls::Controls;
 use scene::Scene;
 
-use glow;
-use glow::*;
-use iced_glow::{Backend, Renderer, Settings, Viewport};
-use iced_glutin::glutin;
-use iced_glutin::glutin::event::{Event, WindowEvent};
-use iced_glutin::glutin::event_loop::{ControlFlow, EventLoop};
-use iced_glutin::{program, Clipboard, Debug, Size};
-use iced_winit::conversion;
-use iced_winit::winit;
-use winit::{dpi::PhysicalPosition, event::ModifiersState};
+use iced_wgpu::{wgpu, Backend, Renderer, Settings, Viewport};
+use iced_winit::{conversion, futures, program, winit, Clipboard, Debug, Size};
+
+use futures::task::SpawnExt;
+use winit::{
+    dpi::PhysicalPosition,
+    event::{Event, ModifiersState, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+};
 
 #[cfg(target_os = "windows")]
-use iced_glutin::glutin::platform::windows::EventLoopExtWindows;
+use iced_winit::winit::platform::windows::EventLoopExtWindows;
 
 #[cfg(target_os = "linux")]
-use iced_glutin::glutin::platform::unix::EventLoopExtUnix;
+use iced_winit::winit::platform::unix::EventLoopExtUnix;
 
 pub async fn module_load(app: &App) -> anyhow::Result<()> {
-    let (gl, event_loop, windowed_context, shader_version) = {
-        let el: EventLoop<()> = glutin::event_loop::EventLoop::new_any_thread();
+    // Initialize winit
+    let event_loop: EventLoop<()> = EventLoop::new_any_thread();
+    let window = winit::window::Window::new(&event_loop).unwrap();
 
-        let wb = glutin::window::WindowBuilder::new()
-            .with_title("OpenGL integration example")
-            .with_inner_size(glutin::dpi::LogicalSize::new(1024.0, 768.0));
-
-        let windowed_context = glutin::ContextBuilder::new()
-            .with_vsync(true)
-            .build_windowed(wb, &el)
-            .unwrap();
-
-        unsafe {
-            let windowed_context = windowed_context.make_current().unwrap();
-
-            let gl = glow::Context::from_loader_function(|s| {
-                windowed_context.get_proc_address(s) as *const _
-            });
-
-            // Enable auto-conversion from/to sRGB
-            gl.enable(glow::FRAMEBUFFER_SRGB);
-
-            // Enable alpha blending
-            gl.enable(glow::BLEND);
-            gl.blend_func(glow::SRC_ALPHA, glow::ONE_MINUS_SRC_ALPHA);
-
-            // Disable multisampling by default
-            gl.disable(glow::MULTISAMPLE);
-
-            (gl, el, windowed_context, "#version 410")
-        }
-    };
-
-    let physical_size = windowed_context.window().inner_size();
+    let physical_size = window.inner_size();
     let mut viewport = Viewport::with_physical_size(
         Size::new(physical_size.width, physical_size.height),
-        windowed_context.window().scale_factor(),
+        window.scale_factor(),
     );
-
     let mut cursor_position = PhysicalPosition::new(-1.0, -1.0);
     let mut modifiers = ModifiersState::default();
-    let mut clipboard = Clipboard::connect(&windowed_context.window());
+    let mut clipboard = Clipboard::connect(&window);
 
-    let mut renderer = Renderer::new(Backend::new(&gl, Settings::default()));
+    // Initialize wgpu
+    let instance = wgpu::Instance::new(wgpu::Backends::PRIMARY);
+    let surface = unsafe { instance.create_surface(&window) };
 
-    let mut debug = Debug::new();
+    let (format, (mut device, queue)) = futures::executor::block_on(async {
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .expect("Request adapter");
 
-    let controls = Controls::new();
-    let mut state =
-        program::State::new(controls, viewport.logical_size(), &mut renderer, &mut debug);
+        (
+            surface
+                .get_preferred_format(&adapter)
+                .expect("Get preferred format"),
+            adapter
+                .request_device(
+                    &wgpu::DeviceDescriptor {
+                        label: None,
+                        features: wgpu::Features::empty(),
+                        limits: wgpu::Limits::default(),
+                    },
+                    None,
+                )
+                .await
+                .expect("Request device"),
+        )
+    });
+
+    {
+        let size = window.inner_size();
+
+        surface.configure(
+            &device,
+            &wgpu::SurfaceConfiguration {
+                usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+                format,
+                width: size.width,
+                height: size.height,
+                present_mode: wgpu::PresentMode::Mailbox,
+            },
+        )
+    };
     let mut resized = false;
 
+    // Initialize staging belt and local pool
+    let mut staging_belt = wgpu::util::StagingBelt::new(5 * 1024);
+    let mut local_pool = futures::executor::LocalPool::new();
+
+    // Initialize scene and GUI controls
+    let scene = Scene::new(&mut device);
+    let controls = Controls::new();
+
+    // Initialize iced
+    let mut debug = Debug::new();
+    let mut renderer = Renderer::new(Backend::new(&mut device, Settings::default(), format));
+
+    let mut state =
+        program::State::new(controls, viewport.logical_size(), &mut renderer, &mut debug);
+
+    // Run event loop
     event_loop.run(move |event, _, control_flow| {
-        let scene = Scene::new(&gl, &shader_version);
+        // You should change this if you want to render continuosly
         *control_flow = ControlFlow::Wait;
 
         match event {
-            Event::LoopDestroyed => return,
             Event::WindowEvent { event, .. } => {
                 match event {
                     WindowEvent::CursorMoved { position, .. } => {
@@ -90,27 +113,24 @@ pub async fn module_load(app: &App) -> anyhow::Result<()> {
                     WindowEvent::ModifiersChanged(new_modifiers) => {
                         modifiers = new_modifiers;
                     }
-                    WindowEvent::Resized(physical_size) => {
+                    WindowEvent::Resized(new_size) => {
                         viewport = Viewport::with_physical_size(
-                            Size::new(physical_size.width, physical_size.height),
-                            windowed_context.window().scale_factor(),
+                            Size::new(new_size.width, new_size.height),
+                            window.scale_factor(),
                         );
 
                         resized = true;
                     }
                     WindowEvent::CloseRequested => {
-                        scene.cleanup(&gl);
-                        *control_flow = ControlFlow::Exit
+                        *control_flow = ControlFlow::Exit;
                     }
-                    _ => (),
+                    _ => {}
                 }
 
                 // Map window event to iced event
-                if let Some(event) = iced_winit::conversion::window_event(
-                    &event,
-                    windowed_context.window().scale_factor(),
-                    modifiers,
-                ) {
+                if let Some(event) =
+                    iced_winit::conversion::window_event(&event, window.scale_factor(), modifiers)
+                {
                     state.queue_event(event);
                 }
             }
@@ -127,42 +147,92 @@ pub async fn module_load(app: &App) -> anyhow::Result<()> {
                     );
 
                     // and request a redraw
-                    windowed_context.window().request_redraw();
+                    window.request_redraw();
                 }
             }
             Event::RedrawRequested(_) => {
                 if resized {
-                    let size = windowed_context.window().inner_size();
+                    let size = window.inner_size();
 
-                    unsafe {
-                        gl.viewport(0, 0, size.width as i32, size.height as i32);
-                    }
+                    surface.configure(
+                        &device,
+                        &wgpu::SurfaceConfiguration {
+                            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+                            format: format,
+                            width: size.width,
+                            height: size.height,
+                            present_mode: wgpu::PresentMode::Mailbox,
+                        },
+                    );
 
                     resized = false;
                 }
 
-                let program = state.program();
-                {
-                    // We clear the frame
-                    scene.clear(&gl, program.background_color());
+                match surface.get_current_texture() {
+                    Ok(frame) => {
+                        let mut encoder =
+                            device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                                label: None,
+                            });
 
-                    // Draw the scene
-                    scene.draw(&gl);
+                        let program = state.program();
+
+                        let view = frame
+                            .texture
+                            .create_view(&wgpu::TextureViewDescriptor::default());
+
+                        {
+                            // We clear the frame
+                            let mut render_pass =
+                                scene.clear(&view, &mut encoder, program.background_color());
+
+                            // Draw the scene
+                            scene.draw(&mut render_pass);
+                        }
+
+                        // And then iced on top
+                        renderer.with_primitives(|backend, primitive| {
+                            backend.present(
+                                &mut device,
+                                &mut staging_belt,
+                                &mut encoder,
+                                &view,
+                                primitive,
+                                &viewport,
+                                &debug.overlay(),
+                            );
+                        });
+
+                        // Then we submit the work
+                        staging_belt.finish();
+                        queue.submit(Some(encoder.finish()));
+                        frame.present();
+
+                        // Update the mouse cursor
+                        window.set_cursor_icon(iced_winit::conversion::mouse_interaction(
+                            state.mouse_interaction(),
+                        ));
+
+                        // And recall staging buffers
+                        local_pool
+                            .spawner()
+                            .spawn(staging_belt.recall())
+                            .expect("Recall staging buffers");
+
+                        local_pool.run_until_stalled();
+                    }
+                    Err(error) => match error {
+                        wgpu::SurfaceError::OutOfMemory => {
+                            panic!("Swapchain error: {}. Rendering cannot continue.", error)
+                        }
+                        _ => {
+                            // Try rendering again next frame.
+                            window.request_redraw();
+                        }
+                    },
                 }
-
-                // And then iced on top
-                renderer.with_primitives(|backend, primitive| {
-                    backend.present(&gl, primitive, &viewport, &debug.overlay());
-                });
-
-                // Update the mouse cursor
-                windowed_context.window().set_cursor_icon(
-                    iced_winit::conversion::mouse_interaction(state.mouse_interaction()),
-                );
-
-                windowed_context.swap_buffers().unwrap();
             }
-            _ => (),
+            _ => {}
         }
     });
 
