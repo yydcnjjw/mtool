593d9fd5308aa1baa9fe40b55ea023fff690543b
yydcnjjw <yydcnjjw@gmail.com>
2021-12-08 18:12:08 +0800

    feat(gterminal): iced integration

--- cli/src/module/mod.rs
+++ cli/src/module/mod.rs
@@ -1,12 +1,12 @@
 mod test;
 mod translate;
-mod gshell;
+mod gterminal;
 
 use crate::app::App;
 
 pub async fn module_load(app: &App) -> anyhow::Result<()> {
     test::module_load(app).await?;
     translate::module_load(app).await?;
-    gshell::module_load(app).await?;
+    gterminal::module_load(app).await?;
     Ok(())
 }
