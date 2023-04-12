2607b40ec0d8ab49097e24b9717d8fd746fbe0f1
yydcnjjw <yydcnjjw@gmail.com>
2023-04-12 16:57:11 +0800

    fix(mtool): clean no-exist module

--- mtool/src/main.rs
+++ mtool/src/main.rs
@@ -4,7 +4,6 @@ fn main() {
         .add_module_group(mtool_system::module())
         .add_module_group(mtool_wgui::module())
         .add_module(mtool_cmder::Module::default())
-        .add_module(mtool_toast::Module::default())
         .add_module(mtool_translate::Module::default())
         .add_module(mtool_dict::Module::default())
         .add_module(mtool_interactive::module())
