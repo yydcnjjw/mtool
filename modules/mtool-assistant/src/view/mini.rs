871a787136ccb2a4e4d155bab0d1cbe561795fcf
yydcnjjw <yydcnjjw@gmail.com>
2025-08-31 22:01:45 +0800

    feat: Add p2p peer count to mini view

--- modules/mtool-assistant/src/view/mini.rs
+++ modules/mtool-assistant/src/view/mini.rs
@@ -1,40 +1,52 @@
-use dioxus::prelude::*;
-use mapp::{
-    anyhow,
-    futures::{StreamExt, TryFutureExt},
-    tracing::warn,
-};
+use std::time::Duration;
 
-use crate::{
-    media::{PlayerEvent, TimedCue},
-    view::component::MediaPlayerControlContext,
+use dioxus::prelude::*;
+use mapp::{anyhow, futures::TryFutureExt, prelude::*, tokio, tracing::warn};
+use mtool_dioxus::{
+    free_icons::{icons::fa_solid_icons::FaNetworkWired, Icon},
+    hooks::{consume_app_context, use_crdt_signal},
 };
+use mtool_p2p as p2p;
+
+use crate::view::component::MediaPlayerControlContext;
 
 #[component]
 pub fn MiniView() -> Element {
     let context = MediaPlayerControlContext::get().suspend()?;
 
-    let mut timed_cue = use_signal(|| TimedCue::default());
+    let timed_cue = use_crdt_signal(context().current_timed_cue);
+    let mut stats = use_signal(|| None);
 
-    use_resource(move || {
+    use_future(move || {
         async move {
-            let mut stream = context().player_event_stream().await?;
-            while let Some(Ok(event)) = stream.next().await {
-                match event {
-                    PlayerEvent::TimedCuesChanged { cue, .. } => {
-                        timed_cue.set(cue);
-                    }
-                    _ => {}
-                }
+            let peer = consume_app_context::<Res<p2p::Peer>>().await;
+
+            let mut tick = tokio::time::interval(Duration::from_secs(5));
+            loop {
+                stats.set(Some(peer.stats().await?));
+                tick.tick().await;
             }
-            Ok::<_, anyhow::Error>(())
         }
-        .unwrap_or_else(|e| warn!("{e:?}"))
+        .unwrap_or_else(|e: anyhow::Error| warn!("{e:?}"))
     });
+
+    let n_peers = stats
+        .as_ref()
+        .map(|stats| stats.gossipsub.all_peers.len())
+        .unwrap_or_default();
 
     rsx! {
         div {
             class: "flex flex-col items-center h-vh my-2 overflow-hidden",
+            button {
+                class: "btn btn-square btn-ghost",
+                Icon {
+                    width: 16,
+                    height: 16,
+                    icon: FaNetworkWired,
+                }
+                "{n_peers}",
+            }
             p {
                 class: "text-2xl truncate",
                 writing_mode: "vertical-lr",
