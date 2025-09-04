39bd92280c09cba58c2c5b3f7075e7a0d3640516
yydcnjjw <yydcnjjw@gmail.com>
2025-09-04 15:19:02 +0800

    fix: Log toast errors

--- modules/mtool-dioxus/src/components/toast.rs
+++ modules/mtool-dioxus/src/components/toast.rs
@@ -1,16 +1,18 @@
 use std::time::Duration;
 
 use dioxus_primitives::toast::{ToastOptions, Toasts};
-use mapp::anyhow;
+use mapp::{anyhow, tracing::warn};
 
 pub fn toast_err<E>(toast: Toasts, e: E)
 where
     E: Into<anyhow::Error>,
 {
+    let e = e.into();
+    warn!("{e:?}");
     toast.error(
         "Error occurred".into(),
         ToastOptions::default()
-            .description(e.into())
+            .description(e)
             .duration(Duration::from_secs(3)),
     );
 }
